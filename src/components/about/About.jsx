1e1a9c19a69f374a3c5b58c92d507fcb3a1ce447
notsugat <sugatsujakhu@gmail.com>
2023-12-31 22:21:41 +0545

    Profile pic changed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,39 +1,39 @@
-import React from "react"
-import "./about.css"
-import ME from "../../assets/person-1.jpg"
-import { FaAward } from "react-icons/fa"
-import { FiUsers } from "react-icons/fi"
-import { VscFolderLibrary } from "react-icons/vsc"
+import React from "react";
+import "./about.css";
+import ME from "../../assets/sugat.webp";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Get to Know</h5>
       <h2>About Me</h2>
 
-      <div className='container about__container'>
-        <div className='about__me'>
-          <div className='about__me-image'>
-            <img src={ME} alt='About Image' />
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
           </div>
         </div>
 
-        <div className='about__content'>
-          <div className='about__cards'>
-            <article className='about__card'>
-              <FaAward className='about__icon' />
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
               <h5>Experience</h5>
               <small>1+ year working</small>
             </article>
 
-            <article className='about__card'>
-              <FiUsers className='about__icon' />
+            <article className="about__card">
+              <FiUsers className="about__icon" />
               <h5>Clients</h5>
               <small>5+ World wide</small>
             </article>
 
-            <article className='about__card'>
-              <VscFolderLibrary className='about__icon' />
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
               <h5>Projects</h5>
               <small>10+ completed project</small>
             </article>
@@ -45,13 +45,13 @@ const About = () => {
             learner and ethusiast who thrive to learn something new everyday.
           </p>
 
-          <a href='#contact' className='btn btn-primary'>
+          <a href="#contact" className="btn btn-primary">
             Let's Talk
           </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,101 +1,104 @@
-import React from "react"
-import "./services.css"
-import { AiOutlineCheck } from "react-icons/ai"
+import React from "react";
+import "./services.css";
+import { AiOutlineCheck } from "react-icons/ai";
 
 function Services() {
   return (
-    <section id='services'>
+    <section id="services">
       <h5>What I offer</h5>
       <h2>Services</h2>
 
-      <div className='container services__container'>
-        <article className='service'>
-          <div className='service__head'>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
             <h3>UI/UX Design</h3>
           </div>
 
-          <ul className='service__list'>
+          <ul className="service__list">
             <li>
-              <AiOutlineCheck className='service__list-icon' />
+              <AiOutlineCheck className="service__list-icon" />
               <p>Creating of good UI interface</p>
             </li>
             <li>
-              <AiOutlineCheck className='service__list-icon' />
+              <AiOutlineCheck className="service__list-icon" />
               <p>Engaging User Experience</p>
             </li>
             <li>
-              <AiOutlineCheck className='service__list-icon' />
+              <AiOutlineCheck className="service__list-icon" />
               <p>Clear Navigation of Webpages</p>
             </li>
           </ul>
         </article>
 
         {/* End of UI/UX */}
-        <article className='service'>
-          <div className='service__head'>
+        <article className="service">
+          <div className="service__head">
             <h3>Web Development</h3>
           </div>
 
-          <ul className='service__list'>
+          <ul className="service__list">
             <li>
-              <AiOutlineCheck className='service__list-icon' />
+              <AiOutlineCheck className="service__list-icon" />
               <p>Single page landing page</p>
             </li>
             <li>
-              <AiOutlineCheck className='service__list-icon' />
+              <AiOutlineCheck className="service__list-icon" />
               <p>Multiple Page website</p>
             </li>
             <li>
-              <AiOutlineCheck className='service__list-icon' />
+              <AiOutlineCheck className="service__list-icon" />
               <p>Creation of Full stack Website</p>
             </li>
             <li>
-              <AiOutlineCheck className='service__list-icon' />
+              <AiOutlineCheck className="service__list-icon" />
               <p>Basic Website using HTML/CSS</p>
+            </li>
+
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Figma to React, Next.js</p>
+            </li>
+
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Hosting your web app in affordable price</p>
             </li>
           </ul>
         </article>
 
         {/* End of web development */}
-        <article className='service'>
-          <div className='service__head'>
+        <article className="service">
+          <div className="service__head">
             <h3>Content Creation</h3>
           </div>
 
-          <ul className='service__list'>
+          <ul className="service__list">
             <li>
-              <AiOutlineCheck className='service__list-icon' />
+              <AiOutlineCheck className="service__list-icon" />
               <p>Successfully handled Facebook Pages</p>
             </li>
             <li>
-              <AiOutlineCheck className='service__list-icon' />
+              <AiOutlineCheck className="service__list-icon" />
               <p>Good Understanding of social Media SEO</p>
             </li>
             <li>
-              <AiOutlineCheck className='service__list-icon' />
+              <AiOutlineCheck className="service__list-icon" />
               <p>
                 Used Digital Marketing Skills, Social Media Marketing Skills
               </p>
             </li>
             <li>
-              <AiOutlineCheck className='service__list-icon' />
+              <AiOutlineCheck className="service__list-icon" />
               <p>
                 Record and edit video for use as social content and digital
                 content
-              </p>
-            </li>
-            <li>
-              <AiOutlineCheck className='service__list-icon' />
-              <p>
-                Promoted targeted content through various social networking
-                sites
               </p>
             </li>
           </ul>
         </article>
       </div>
     </section>
-  )
+  );
 }
 
-export default Services
+export default Services;
